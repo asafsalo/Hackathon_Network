ce3ad6a1f1ae4aee550fb4980923b046d9349281
asafsalo <3asafi51@gmail.com>
2020-12-31 02:17:48 +0200

    asdas

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 import struct
 from getch import _Getch
-from scapy.all import*
+# from scapy.all import*
 
 
 class Client:
@@ -11,13 +11,11 @@ class Client:
         self.magic_cookie = 0xfeedbeef # 0xfeedbeef
         self.offer_message_type = 0x2
 
-
         # Client Global Parameters
-        self.ip_network = get_if_addr('eth1')
+        self.ip_network = "192.168.0.183" # get_if_addr('eth1')
         self.udp_listen_port = 13117
-        self.buffer_size = 8
+        self.buffer_size = 20
         self.team_name = "The Secrets\n"
-
 
     def listen_state(self):
         """ Bind the udp port - 13117 """
@@ -35,13 +33,11 @@ class Client:
             if len(unpack_message) == 3 and unpack_message[0] == self.magic_cookie and unpack_message[1] == self.offer_message_type:
                 return unpack_message[2]
 
-
     def connect_server_state(self, server_port):
         """ Try to connect the server and return a socket if succeed """
         tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         tcp_socket.connect((self.ip_network, server_port))
         return tcp_socket
-
 
     def send_details_to_server(self, sock_tcp):
         """ Send Team-Name """
@@ -49,18 +45,16 @@ class Client:
         sock_tcp.recv(self.buffer_size)
         self.game_state(sock_tcp)
 
-
     def game_state(self, sock_tcp):
         while True:
-            char = _Getch()
-            sock_tcp.send(char)
             try:
+                char = _Getch()
+                sock_tcp.send(char)
                 message = sock_tcp.recv(self.buffer_size)
-                if(len(message) == 0):
+                if len(message) == 0:
                     break
                 message_to_print = message.decode("utf-8")
                 print(message_to_print)
-                
             except: 
                 self.listen_state()
 
@@ -71,5 +65,5 @@ if __name__ == "__main__":
     while True:
         server_port = client.listen_state()
         connection = client.connect_server_state(server_port)
-        if connection != None:
+        if connection is not None:
             client.send_details_to_server(connection)
